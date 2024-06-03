41b4b911a1e9b2a87b521ce1bc63d7b10b3ba664
Cmaxium <167007251+Cmaxium@users.noreply.github.com>
2024-06-03 11:03:38 -0230

    Create Person.java

---
+++ Person.java
@@ -0,0 +1,19 @@
+// Person.java
+public class Person {
+    private String firstName;
+    private String lastName;
+    private Address address;
+
+    // Constructor
+    public Person(String firstName, String lastName, Address address) {
+        this.firstName = firstName;
+        this.lastName = lastName;
+        this.address = address;
+    }
+
+    // toString method
+    @Override
+    public String toString() {
+        return firstName + " " + lastName + ", " + address;
+    }
+}
